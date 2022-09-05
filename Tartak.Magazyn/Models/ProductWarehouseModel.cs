0bda3d1074ceb999dec0b5dc2d3c4fd4ba4b4d26
adamfed7 <adamfedor5@gmail.com>
2022-09-05 21:56:28 +0200

    Creating model and migrations for ef database

---
+++ Tartak.Magazyn/Models/ProductWarehouseModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tartak.Magazyn.Models
+{
+    public class ProductWarehouseModel
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public decimal PurchasePrice { get; set; }
+        public uint QuantityInWarehouse { get; set; }
+
+    }
+}
