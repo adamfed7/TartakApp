5a74eaf2b8b29b202e28c50df5c32ea2563a45e4
adamfed7 <91409021+adamfed7@users.noreply.github.com>
2022-09-10 19:33:26 +0200

    Transfer of classes to Server Models

--- Tartak.WebApp/Server/Controllers/UserController.cs
+++ Tartak.WebApp/Server/Controllers/UserController.cs
@@ -3,8 +3,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
 using System.Security.Claims;
+using Tartak.WebApp.Models;
 using Tartak.WebApp.Server.Data;
-using Tartak.WebApp.Shared.Models;
 
 namespace Tartak.WebApp.Server.Controllers
 {
